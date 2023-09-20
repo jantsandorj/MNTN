1c3747cbe6aba8bbc94ebd21deb88a380ee6a501
jantsandorj <102676794+kieunMoon@users.noreply.github.com>
2023-09-20 10:49:08 +0800

    scroller

--- assets/js/contents.js
+++ assets/js/contents.js
@@ -1,75 +1,72 @@
-// gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(Draggable);
+const blocks = document.querySelectorAll(".block");
+blocks.forEach((block, index) => {
+  const Reverse_contentWrap = block.querySelector(
+    ".reverse_block .content_wrap"
+  );
+  const Reverse_blockImg = block.querySelector(".reverse_block img");
+  const contentWrap = block.querySelector(".trigger_block .content_wrap");
+  const blockImg = block.querySelector(".trigger_block img");
+  gsap.from(contentWrap, {
+    opacity: 0,
+    x: -100,
+    duration: 1,
+    scrollTrigger: {
+      trigger: contentWrap,
+      start: "top 80%",
+      end: "bottom 20%",
+      scrub: true,
+    },
+    delay: index * 0.5,
+  });
+  gsap.from(blockImg, {
+    opacity: 0,
+    x: 100,
+    duration: 1,
+    scrollTrigger: {
+      trigger: blockImg,
+      start: "top 80%",
+      end: "bottom 20%",
+      scrub: true,
+    },
+    delay: index * 0.5,
+  });
+  gsap.from(Reverse_contentWrap, {
+    opacity: 0,
+    x: 100,
+    duration: 1,
+    scrollTrigger: {
+      trigger: Reverse_contentWrap,
+      start: "top 80%",
+      end: "bottom 20%",
+      scrub: true,
+    },
+    delay: index * 0.5,
+  });
+  gsap.from(Reverse_blockImg, {
+    opacity: 0,
+    x: -100,
+    duration: 1,
+    scrollTrigger: {
+      trigger: Reverse_blockImg,
+      start: "top 80%",
+      end: "bottom 20%",
+      scrub: true,
+    },
+    delay: index * 0.5,
+  });
+});
 
-// gsap.from(".trigger_block img", {
-//   x: 100,
-//   opacity: 0,
-//   duration: 5,
-//   scrollTrigger: {
-//     trigger: ".block",
-//     toggleActions: "restart pause reverse none",
-//     markers: true,
-//   },
-// });
-// gsap.from(".trigger_block .content_wrap", {
-//   x: -100,
-//   opacity: 0,
-//   duration: 5,
-//   scrollTrigger: {
-//     trigger: ".block",
-//     toggleActions: "restart pause reverse none",
-//     markers: true,
-//   },
-// });
-// gsap.from(".reverse_block .content_wrap", {
-//   x: 100,
-//   opacity: 0,
-//   duration: 5,
-//   scrollTrigger: {
-//     trigger: ".reverse_block",
-//     toggleActions: "restart pause reverse none",
-//     markers: true,
-//   },
-// });
-// gsap.from(".reverse_block img", {
-//   x: -100,
-//   opacity: 0,
-//   duration: 5,
-//   scrollTrigger: {
-//     trigger: ".reverse_block",
-//     toggleActions: "restart pause reverse none",
-//     markers: true,
-//   },
-// });
-
-document.querySelectorAll(".block").forEach((section) => {
-  const q = gsap.utils.selector(section);
-  gsap
-    .timeline({
-      scrollTrigger: {
-        trigger: section,
-        toggleActions: "restart pause reverse none",
-        scrub: 5,
-        start: "top 50%",
-        end: "top 100%",
-        markers: true,
-      },
-      ease: Elastic.easeOut,
-      duration: 3,
-    })
-    .from(q(".trigger_block img"), {
-      x: 100,
-      opacity: 0,
-    })
-    .from(q(".trigger_block .content_wrap"), {
-      x: -100,
-      opacity: 0,
-    })
-    .from(q(".reverse_block img"), {
-      x: -100,
-      opacity: 0,
-    })
-    .from(q(".reverse_block .content_wrap"), {
-      x: 100,
-      opacity: 0,
-    });
+const circle = document.querySelector(".scroller");
+gsap.to(circle, {
+  y: 180,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".index-contents",
+    start: "top 80%",
+    end: "bottom bottom",
+    scrub: 1,
+    markers: true,
+  },
 });
